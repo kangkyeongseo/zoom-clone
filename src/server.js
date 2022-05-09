57d0295521e83f14589dc6f570f0e33353ac4254
kangkyeongseo <kks_big@naver.com>
2022-05-09 16:23:22 +0900

    Chat with WebSockets

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,6 +10,34 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost${PORT}`);
-app.listen(PORT, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disonnected from Browser"));
+  socket.on("message", (message) => {
+    const translateMessage = message.toString("utf8");
+    const parsedMsg = JSON.parse(translateMessage);
+    switch (parsedMsg.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket.nickname}: ${parsedMsg.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = parsedMsg.payload;
+        break;
+    }
+  });
+});
+
+server.listen(PORT, handleListen);
